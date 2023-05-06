75d97b4b27b3250c8586baf2af796a007335d701
Mert <128643904+mert-mktrnk@users.noreply.github.com>
2023-05-06 11:30:27 +0300

    Update Car.ino

--- Car/Car.ino
+++ Car/Car.ino
@@ -1,55 +1,3 @@
-const int sensor1 = D12;
-const int sensor2 = D13;
-const int sensor3 = D14;
-const int sensor4 = D15;
-
-#define FRONT_LEFT D12 // pin 2 for front-left sensor
-#define FRONT_RIGHT D13 // pin 3 for front-right sensor
-#define REAR_LEFT D14 // pin 4 for rear-left sensor
-#define REAR_RIGHT D15 // pin 5 for rear-right sensor
-#define BREAK 8 // pin 8 to apply break or turn LED ON
-#define FRONT_OB 9 // pin 9 when obstacle is at front
-#define REAR_OB 10 // pin 10 when obstacle is at rear
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(FRONT_LEFT, INPUT);//define front-left input pin
-  pinMode(FRONT_RIGHT, INPUT);//define front-right input pin
-  // 4 Infrared Obstacle code for Robojax.com
-  pinMode(REAR_LEFT, INPUT);//define rear-left input pin
-  pinMode(REAR_RIGHT, INPUT);//define rear-right input pin
-}
-
-void loop() 
-{
-  int FR = digitalRead(FRONT_RIGHT);// read FRONT_LEFT sensor
-  int FL = digitalRead(FRONT_LEFT);// read FRONT_RIGHT sensor
-  int RR = digitalRead(REAR_RIGHT);// read REAR_RIGHT sensor
-  int RL = digitalRead(REAR_LEFT);// read REAR_LEFT sensor   
-
-  if( FR == LOW || FL == LOW)
-  {
-    Serial.println("1");
-  }
-  else
-  {
-    Serial.println("2");
-  }
-
-
-  if( RR == LOW || RL == LOW )
-  {
-    Serial.println("3");
-  }
-  else
-  {
-    Serial.println("4");
-  }
-
-  delay(200);
-}
-
-/*
 int ENA = D9;
 int ENB = A5;
 
@@ -154,4 +102,3 @@ static void stop(void)
   digitalWrite(IN2, LOW);
   digitalWrite(IN4, LOW);
 }
-*/
